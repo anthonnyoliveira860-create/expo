b9b47f7326c9fdb123cdaa9f24f5d9a0f7f847a8
Hassan Khan <hassan@expo.dev>
2026-04-30 19:15:48 +0100

    test(@expo/cli): fix server loader metadata test case

--- apps/router-e2e/__e2e__/server-loader/app/meta.tsx
+++ apps/router-e2e/__e2e__/server-loader/app/meta.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData, useLocalSearchParams, usePathname } from 'expo-router';
 import Head from 'expo-router/head';
+import type { Metadata } from 'expo-router/server';
 import { Suspense } from 'react';
 
 import { Container } from '../components/Container';
@@ -7,12 +8,23 @@ import { Loading } from '../components/Loading';
 import { Table, TableRow } from '../components/Table';
 import { SiteLinks, SiteLink } from '../components/SiteLink';
 
+export function generateMetadata() {
+  return {
+    title: 'Meta page',
+    description: 'Meta tag testing',
+    keywords: 'expo-router,loaders,meta',
+    authors: {
+      name: 'Expo',
+    },
+  } satisfies Metadata;
+}
+
 export async function loader() {
   return {
     title: 'Meta page',
     description: 'Meta tag testing',
     keywords: 'expo-router,loaders,meta',
-    author: 'Expo'
+    author: 'Expo',
   };
 }
 
@@ -31,13 +43,6 @@ const MetaScreen = () => {
 
   return (
     <>
-      <Head>
-        <title>{data.title}</title>
-        <meta name="description" content={data.description} />
-        <meta name="keywords" content={data.keywords} />
-        <meta name="author" content={data.author} />
-      </Head>
-
       <Table>
         <TableRow label="Pathname" value={pathname} testID="pathname-result" />
         <TableRow label="Local Params" value={localParams} testID="localparams-result" />
@@ -50,4 +55,4 @@ const MetaScreen = () => {
       </SiteLinks>
     </>
   );
-}
+};
